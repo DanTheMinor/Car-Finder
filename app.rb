f7ec45cda937fe02e018c0809ea5b808ece1dd40
Dan Minor & james robert williams <student@epicodus.com>
2015-04-29 12:13:15 -0700

    update sinatra with dealership pages, removed multiple vehicle pages.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/dealership')
 require('./lib/vehicle')
 also_reload('lib/**/*.rb')
 
@@ -7,13 +8,34 @@ get ('/') do
   erb(:index)
 end
 
-get('/vehicles') do
-  @vehicles = Vehicle.all()
-  erb(:vehicles)
+get('/dealerships/new') do
+  erb(:dealerships_form)
 end
 
-get('/vehicles/new') do
-  erb(:vehicle_form)
+get('/dealerships') do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
+end
+
+post('/dealerships') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id"))
+  erb(:vehicle)
+end
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+get('/dealerships/:id/vehicles/new') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership_vehicles_form)
 end
 
 post('/vehicles') do
@@ -22,10 +44,15 @@ post('/vehicles') do
   year = params.fetch("year")
   @vehicle = Vehicle.new(make, model, year)
   @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
   erb(:success)
 end
-
-get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch("id"))
-  erb(:vehicle)
-end
+# get('/vehicles') do
+#   @vehicles = Vehicle.all()
+#   erb(:vehicles)
+# end
+#
+# get('/vehicles/new') do
+#   erb(:vehicle_form)
+# end
