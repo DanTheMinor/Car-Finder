0468cf8f92eb24a58f70a1a3dc72ca3b7cd9a8bb
Dan Minor & james robert williams <student@epicodus.com>
2015-04-29 11:15:03 -0700

    update dealership ruby logic to include name. pass first spec to test for name of dealership

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,22 @@
+require('rspec')
+require('dealership')
+
+describe(Dealership) do
+  # before() do
+  #   Dealership.clear()
+  # end
+  describe('#name') do
+    it("returns the name of the dealership") do
+      test_dealer = Dealership.new("Bob's")
+      expect(test_dealer.name()).to(eq("Bob's"))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the dealership') do
+      test_dealership = Dealership.new("Bob's")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+
+end
